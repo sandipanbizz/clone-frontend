75aad05a670cbf5ff346dca3bbbf1b3010f7264e
sandipanbizz <sandip.anbizz@gmail.com>
2025-10-11 15:32:22 +0530

    change the url

--- src/BASE_URL.jsx
+++ src/BASE_URL.jsx
@@ -1,9 +1,9 @@
 // export const BASE_URL = "http://3.108.65.195:8000/";
 
-// export const BASE_URL = "http://localhost:3000/";
+export const BASE_URL = "http://localhost:3000/";
 // export const BASE_URL = "http://135.235.160.172:3000/";
 
-export const BASE_URL = "https://api.chembizz.in/";
+// export const BASE_URL = "https://api.chembizz.in/";
 
 
 
