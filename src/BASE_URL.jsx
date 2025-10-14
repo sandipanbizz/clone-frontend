2af493630e8d07b7996de3e6f6fe018f1575c0d3
sandipanbizz <sandip.anbizz@gmail.com>
2025-10-14 14:12:53 +0530

    Update BASE_URL.jsx

--- src/BASE_URL.jsx
+++ src/BASE_URL.jsx
@@ -1,6 +1,6 @@
 // export const BASE_URL = "http://3.108.65.195:8000/";
 
-export const BASE_URL = "https://chembizz-backend.vercel.app/";
+export const BASE_URL = "https://chembizz-backend.onrender.com/";
 // export const BASE_URL = "http://135.235.160.172:3000/";
 
 // export const BASE_URL = "https://api.chembizz.in/";
